1f2853c76e6b4a1277952cdc37e54f44e09f9fcf
Manuel Zimmermann <manuel@mauzi.eu>
2017-02-05 20:31:46 +0100

    Refactoring

--- src/main/java/me/huntro/Main.java
+++ src/main/java/me/huntro/Main.java
@@ -1,25 +1,11 @@
 package me.huntro;
 
-import java.net.URL;
-
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Cursor;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseButton;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.stage.WindowEvent;
 
 
 public class Main extends Application
 {
-	private double x;
-	private double y;
-	
 	public static void main(String[] args)
 	{
 		launch(args);
@@ -28,80 +14,6 @@ public class Main extends Application
 	@Override
 	public void start(Stage stage) throws Exception
 	{
-		Pane root = new Pane();
-		
-		configStage(stage, root);
-		
-		new Thread(new SlideShow(stage, root)).start();
-	}
-	
-	private void configStage(Stage stage, Pane root)
-	{
-		Platform.runLater(() ->
-		{
-			try
-			{
-				URL url = new URL("https://upload.wikimedia.org/wikipedia/en/3/39/Pokeball.PNG");
-				
-				stage.getIcons().add(new Image(ProxyHandler.openConnection(url).getInputStream()));
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
-		});
-		
-		stage.setTitle("Pokemon");
-		stage.initStyle(StageStyle.TRANSPARENT);
-		stage.setAlwaysOnTop(true);
-		stage.centerOnScreen();
-		
-		stage.addEventHandler(KeyEvent.KEY_RELEASED, e ->
-		{
-			if(e.getCode() == KeyCode.ESCAPE)
-			{
-				stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
-			}
-		});
-		stage.setOnCloseRequest(e -> System.exit(0));
-		
-		stage.setScene(getScene(stage, root));
-		stage.sizeToScene();
-	}
-	
-	private Scene getScene(Stage stage, Pane root)
-	{
-		Scene scene = new Scene(root);
-		
-		scene.setCursor(Cursor.OPEN_HAND);
-		scene.setFill(null);
-		
-		scene.setOnMousePressed(e ->
-		{
-			if(e.getButton() == MouseButton.PRIMARY)
-			{
-				x = stage.getX() - e.getScreenX();
-				y = stage.getY() - e.getScreenY();
-				
-				scene.setCursor(Cursor.CLOSED_HAND);
-			}
-		});
-		scene.setOnMouseDragged(e ->
-		{
-			if(e.getButton() == MouseButton.PRIMARY)
-			{
-				stage.setX(e.getScreenX() + x);
-				stage.setY(e.getScreenY() + y);
-			}
-		});
-		scene.setOnMouseReleased(e ->
-		{
-			if(e.getButton() == MouseButton.PRIMARY)
-			{
-				scene.setCursor(Cursor.OPEN_HAND);
-			}
-		});
-		
-		return scene;
+		new SlideShow(stage).start();
 	}
 }
--- src/main/java/me/huntro/SlideShow.java
+++ src/main/java/me/huntro/SlideShow.java
@@ -1,7 +1,14 @@
 package me.huntro;
 
+import java.net.URL;
+
 import javafx.application.Platform;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
@@ -9,59 +16,132 @@ import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 
 
-public class SlideShow implements Runnable
+public class SlideShow
 {
 	public static final long INTERVAL = 2000L;
 	public static final BorderWidths BORDER_WIDTH = new BorderWidths(50);
 	public static final CornerRadii RADII = new CornerRadii(50);
 	
 	private Stage stage;
-	private Pane root;
+	private Pane root = new Pane();
 	
-	public SlideShow(Stage stage, Pane root)
+	private double x;
+	private double y;
+	
+	public SlideShow(Stage stage)
 	{
 		this.stage = stage;
-		this.root = root;
-	}
-	
-	@Override
-	public void run()
-	{
-		ImageHandler imgHandler = new ImageHandler();
-		ImageView imgView = new ImageView();
 		
-		imgView.setSmooth(true);
-		root.getChildren().add(imgView);
-		
-		while(true)
+		Platform.runLater(() ->
 		{
-			Image img = imgHandler.getNextImage();
-			imgView.setImage(img);
-			
-			Platform.runLater(() ->
-			{
-				root.setBorder(new Border(new BorderStroke(img.getAverageColor(), BorderStrokeStyle.SOLID, RADII, BORDER_WIDTH)));
-				
-				if(!stage.isShowing())
-				{
-					stage.show();
-				}
-				
-				stage.setWidth(img.getWidth());
-				stage.setHeight(img.getHeight());
-				
-			});
-			
 			try
 			{
-				Thread.sleep(INTERVAL);
+				URL url = new URL("https://upload.wikimedia.org/wikipedia/en/3/39/Pokeball.PNG");
+				
+				stage.getIcons().add(new Image(ProxyHandler.openConnection(url).getInputStream()));
 			}
-			catch(InterruptedException e)
+			catch(Exception e)
 			{
 				e.printStackTrace();
 			}
-		}
+		});
+		
+		stage.setTitle("Pokemon");
+		stage.initStyle(StageStyle.TRANSPARENT);
+		stage.setAlwaysOnTop(true);
+		stage.centerOnScreen();
+		
+		stage.addEventHandler(KeyEvent.KEY_RELEASED, e ->
+		{
+			if(e.getCode() == KeyCode.ESCAPE)
+			{
+				stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
+			}
+		});
+		stage.setOnCloseRequest(e -> System.exit(0));
+		
+		stage.setScene(getScene());
+		stage.sizeToScene();
+	}
+	
+	public void start()
+	{
+		new Thread(() ->
+		{
+			ImageHandler imgHandler = new ImageHandler();
+			ImageView imgView = new ImageView();
+			
+			imgView.setSmooth(true);
+			root.getChildren().add(imgView);
+			
+			while(true)
+			{
+				Image img = imgHandler.getNextImage();
+				imgView.setImage(img);
+				
+				Platform.runLater(() ->
+				{
+					root.setBorder(new Border(new BorderStroke(img.getAverageColor(), BorderStrokeStyle.SOLID, RADII, BORDER_WIDTH)));
+					
+					if(!stage.isShowing())
+					{
+						stage.show();
+					}
+					
+					stage.setWidth(img.getWidth());
+					stage.setHeight(img.getHeight());
+					
+				});
+				
+				try
+				{
+					Thread.sleep(INTERVAL);
+				}
+				catch(InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+	
+	private Scene getScene()
+	{
+		Scene scene = new Scene(root);
+		
+		scene.setCursor(Cursor.OPEN_HAND);
+		scene.setFill(null);
+		
+		scene.setOnMousePressed(e ->
+		{
+			if(e.getButton() == MouseButton.PRIMARY)
+			{
+				x = stage.getX() - e.getScreenX();
+				y = stage.getY() - e.getScreenY();
+				
+				scene.setCursor(Cursor.CLOSED_HAND);
+			}
+		});
+		scene.setOnMouseDragged(e ->
+		{
+			if(e.getButton() == MouseButton.PRIMARY)
+			{
+				stage.setX(e.getScreenX() + x);
+				stage.setY(e.getScreenY() + y);
+			}
+		});
+		scene.setOnMouseReleased(e ->
+		{
+			if(e.getButton() == MouseButton.PRIMARY)
+			{
+				scene.setCursor(Cursor.OPEN_HAND);
+			}
+		});
+		
+		return scene;
 	}
 }
